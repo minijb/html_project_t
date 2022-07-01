dced771b79882c1877d71863350f0dd96b6eadec
minijb <1414309848@qq.com>
2022-07-01 20:33:24 +0800

    2022/7/1

--- js/select_by_age.js
+++ js/select_by_age.js
@@ -8,31 +8,31 @@ $(function() {
         
     });
 
-    $(".img_bu").on('click',function(){
-        let me = $(this)
-        let a = me.attr("con");
-        if(a==0){
-            me.attr("con",1);
-            me.attr("src","../Img/select_by_age/u251_selected.svg")
-        }else{
-            me.attr("con",0);
-            me.attr("src","../Img/select_by_age/u251.svg")
-        }
-    });
+    // $(".img_bu").on('click',function(){
+    //     let me = $(this)
+    //     let a = me.attr("con");
+    //     if(a==0){
+    //         me.attr("con",1);
+    //         me.attr("src","../Img/select_by_age/u251_selected.svg")
+    //     }else{
+    //         me.attr("con",0);
+    //         me.attr("src","../Img/select_by_age/u251.svg")
+    //     }
+    // });
 
-    $(".img_bu").mouseover(function(){
-        let me = $(this)
-        let a = me.attr("con")
-        if(a == 0){
-            me.attr("src","../Img/select_by_age/u251_mouseOver.svg")
-        }
-    })
-    $(".img_bu").mouseout(function(){
-        let me = $(this)
-        let a = me.attr("con")
-        if(a == 0){
-            me.attr("src","../Img/select_by_age/u251.svg")
-        }
-    })
+    // $(".img_bu").mouseover(function(){
+    //     let me = $(this)
+    //     let a = me.attr("con")
+    //     if(a == 0){
+    //         me.attr("src","../Img/select_by_age/u251_mouseOver.svg")
+    //     }
+    // })
+    // $(".img_bu").mouseout(function(){
+    //     let me = $(this)
+    //     let a = me.attr("con")
+    //     if(a == 0){
+    //         me.attr("src","../Img/select_by_age/u251.svg")
+    //     }
+    // })
 
 });
